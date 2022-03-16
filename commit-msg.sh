35daec9db65c22ef5c8b5a5bacf6521267ac09cc
Chu Tingting <tingting.chu@thoughtworks.com>
2022-03-16 11:41:45 +0800

    add husky eslint prettier

--- commit-msg.sh
+++ commit-msg.sh
@@ -3,7 +3,7 @@
 error_msg="Aborting commit. Your commit message format needs to be corrected"
 commit_message=$(cat $1)
 check_commit_msg() {
-  local REGEX='^\[((FEAT)|(FIX)|(REFACTOR)|(BE-[0-9]{0,3})|(BE-[0-9]{2,3}))\].+$'
+  local REGEX='^\[((FEAT)|(FIX)|(REFACTOR)|(BEUI-[0-9]{0,3})|(BEUI-[0-9]{2,3}))\].+$'
   if [[ ! $commit_message =~ $REGEX ]]; then
     echo >&2 $error_msg
     echo your message: $commit_message
