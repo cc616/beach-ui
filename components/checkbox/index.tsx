08ecf0b864a5984bc451592d98f1ffa411e2741a
sirui yang <siri_yang@163.com>
2022-03-23 15:25:26 +0800

    update checkBox

--- components/checkbox/index.tsx
+++ components/checkbox/index.tsx
@@ -1,13 +1,18 @@
 import {FC, useState} from 'react';
 import cls from "classnames";
 
+import './index.less';
+
 export interface CheckBoxProps {
+  type: 'default' | 'round';
+  size: 'lg' | 'default' | 'sm';
+  color: 'red' | 'default';
   disabled?: boolean;
 }
 
 const prefixCls = 'beach-checkBox';
 
-const CheckBox:FC<CheckBoxProps> = ({disabled = false}:CheckBoxProps) => {
+const CheckBox:FC<CheckBoxProps> = ({disabled = false, type='default', size='default', color}:CheckBoxProps) => {
 
   const [checked,setChecked]=useState(false)
   const handleChange = () => {
@@ -15,7 +20,9 @@ const CheckBox:FC<CheckBoxProps> = ({disabled = false}:CheckBoxProps) => {
   }
 
 return(
-  <input onChange={handleChange} type="checkbox" checked={checked} disabled={disabled} className={cls(prefixCls, `${prefixCls}-${disabled}`)}/>
+  <div className={`${prefixCls}-${color}`}>
+    <input onChange={handleChange} type={type === 'default' ? 'checkbox' : 'radio'} checked={checked} disabled={disabled} className={cls(prefixCls, `${prefixCls}-${type}`, { [`${prefixCls}-${size}`]: size !== 'default' })}/>
+  </div>
 )
 }
 
--- stories/CheckBox.stories.tsx
+++ stories/CheckBox.stories.tsx
@@ -5,11 +5,31 @@ export default {
   title: 'Example/CheckBox',
   component: CheckBox,
   argTypes: {
+    type: {
+      name: 'type',
+      defaultValue: 'default',
+      description: 'type',
+      control: 'select',
+      options: [ "default","round"],
+    },
+    color: {
+      name: 'color',
+      defaultValue: 'default',
+      description: 'color',
+      control: 'select',
+      options: [ "default","red"],
+    },
     disabled: {
       description: 'unchecked',
       defaultValue: false,
       control: { type: 'boolean' },
     },
+    size: {
+      description:' size',
+      defaultValue: 'default',
+      control: 'select',
+      options: [ "default","sm", "lg"],
+    }
   },
 } as Meta;
 
@@ -24,4 +44,5 @@ const BasicTemplate: Story<CheckBoxProps> = (args) => {
 export const Basic = BasicTemplate.bind({});
 Basic.args = {
   disabled: false,
+  type: 'default',
 };
